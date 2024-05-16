1abe7eb5a4be5dadaecfeb49096ffc3e38f84a0c
FranchiquiJ <167950323+FranchiquiJ@users.noreply.github.com>
2024-05-16 08:10:26 +0200

    f 	deleted:    app/entrypoint.sh 	modified:   scanTexto/__pycache__/utils.cpython-312.pyc 	modified:   scanTexto/models.py 	modified:   scanTexto/utils.py

--- scanTexto/utils.py
+++ scanTexto/utils.py
@@ -1,33 +1,36 @@
-import cv2
+import skimage.io
+import skimage.color
+import skimage.filters
 import pytesseract
 
-def scanTexto_func():
+def scanTexto_func(imagePath):
     """
     Extrae texto de una imagen y lo devuelve como cadena.
+
+    Argumentos:
+        imagePath (str): Ruta al archivo de imagen a escanear.
+
     Retorno:
         str: El texto extraído de la imagen.
     """
-    # Ruta de la imagen predefinida
-    imagePath = "ruta/a/la/imagen.jpg"
-    
-    # Cargar la imagen
-    img = cv2.imread(imagePath)
-    
-    # Comprobar si la imagen se leyó correctamente
-    if img is None:
-        raise Exception("Error loading image: {}".format(imagePath))
-    
+
+    # Cargar la imagen utilizando scikit-image
+    img = skimage.io.imread(imagePath)
+
     # Convertir la imagen a escala de grises
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
-    # Aplicar umbrales para convertir a una imagen binaria
-    threshold_img = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-    
+    gray = skimage.color.rgb2gray(img)
+
+    # Aplicar umbralización para convertir a una imagen binaria
+    threshold_img = gray > skimage.filters.threshold_otsu(gray)
+
     # Establecer la ruta de Tesseract
     pytesseract.pytesseract.tesseract_cmd = r'C:/Archivos de programa/Tesseract-OCR/tesseract.exe'
-    
+
+    # Establecer el idioma del texto
+    text = pytesseract.image_to_string(img, config='--psm 10 lang=es')
+
     # Extraer texto usando pytesseract
     text = pytesseract.image_to_string(threshold_img)
-    
+
     # Devolver el texto extraído
     return text
